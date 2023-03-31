90b8d502c7b357d70a29b70d4bcd835aa7e351d8
Co van Leeuwen <coo@covle.com>
2023-03-31 21:40:06 +0700

    made it respond to IMs

--- src/SlackBotApp.ts
+++ src/SlackBotApp.ts
@@ -1,4 +1,4 @@
-import { App, GenericMessageEvent } from "@slack/bolt";
+import { App, GenericMessageEvent, SayFn } from "@slack/bolt";
 import { OpenAiClient } from "./OpenAi";
 
 export class SlackBotApp {
@@ -22,52 +22,63 @@ export class SlackBotApp {
     return this.app.start();
   }
 
+  private stripMentions(text: string) {
+    return text.replace(`<@${this.botUserId}>`, '').trim();
+  }
+
+  private async respondWithChatCompletion(m: GenericMessageEvent, say: SayFn) {
+    const text = this.stripMentions(m.text);
+
+    console.log("Receiving message:", text);
+
+    const prompts: string[] = [];
+
+    // If the message is a top level comment
+    if (!m.thread_ts) {
+      prompts.push(text);
+    } else {
+      // Get all the messages in the thread
+      const conversationId = m.channel;
+      const threadId = m.thread_ts;
+
+      const result = await this.app.client.conversations.replies({
+        channel: conversationId,
+        ts: threadId,
+      });
+      const messages = result.messages || [];
+
+      prompts.push(...messages.map((m) => this.stripMentions(m.text)));
+    }
+
+    try {
+      const response = await this.openai.getChat(prompts);
+
+      say({
+        text: response,
+        thread_ts: m.thread_ts || m.ts,
+      });
+    } catch (error) {
+      console.error(error);
+      // Handle any errors that may occur
+      await say({
+        text: "Oops! Something went wrong. Please try again later.",
+        thread_ts: m.thread_ts || m.ts,
+      });
+    }
+  }
+
   private setupCallbacks() {
-    this.app.message(
-      `<@${this.botUserId}>`,
-      async ({ say, message, client }) => {
-        const m = message as GenericMessageEvent;
+    this.app.message(async ({ message, say }) => {
+      const m = message as GenericMessageEvent;
 
-        console.log("Receiving message:", m.text);
+      if (m.channel_type === "im") await this.respondWithChatCompletion(m, say);
+    });
 
-        const prompts: string[] = [];
+    this.app.message(`<@${this.botUserId}>`, async ({ say, message }) => {
+      const m = message as GenericMessageEvent;
 
-        // If the message is a top level comment
-        if (!m.thread_ts) {
-          prompts.push(m.text);
-        } else {
-          // Get all the messages in the thread
-          const conversationId = message.channel;
-          const threadId = m.thread_ts;
-
-          const result = await client.conversations.replies({
-            channel: conversationId,
-            ts: threadId,
-          });
-          const messages = result.messages || [];
-
-          prompts.push(...messages.map((m) => m.text));
-        }
-
-        try {
-          const response = await this.openai.getChat(prompts);
-
-          say({
-            text: response,
-            thread_ts: m.thread_ts || undefined,
-            channel: m.thread_ts ? undefined : m.channel,
-          });
-        } catch (error) {
-          console.error(error);
-          // Handle any errors that may occur
-          await say({
-            text: "Oops! Something went wrong. Please try again later.",
-            thread_ts: m.thread_ts || undefined,
-            channel: m.thread_ts ? undefined : m.channel,
-          });
-        }
-      }
-    );
+      await this.respondWithChatCompletion(m, say);
+    });
 
     this.app.command("/image", async ({ command, ack, say }) => {
       console.log("Generating image", command.text);
@@ -77,10 +88,10 @@ export class SlackBotApp {
 
         const text = command.text.replace(`<@${this.botUserId}>`, "").trim();
         const image_url = await this.openai.getImage(text);
-        const summary = 'Your Image'
+        const summary = "Your Image";
 
         say({
-          text: 'Here it is!',
+          text: "Here it is!",
           blocks: [
             {
               type: "image",
